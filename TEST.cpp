c7e0f7bdec0dc640a36565d8ccf09b7ba1ec11da
Surakattula Rohith <74316672+Surakattula-Rohith@users.noreply.github.com>
2021-08-23 08:51:42 +0530

    Add files via upload

---
+++ TEST.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	while(true){
+	    int x ;
+	    cin >> x ;
+	    if(x == 42) break ;
+	    std::cout <<  x << std::endl;
+	}
+	return 0;
+}
